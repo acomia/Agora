e2bd05374a8a927afbcfaea3fa97dcd3a8cfc4b0
rjbaes <rjbaes@gmail.com>
2019-11-14 13:52:43 +0800

    dynamic token on searching doctor

--- index.js
+++ index.js
@@ -5,6 +5,5 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import StackNavigator from './src/routes/StackNavigator'
-import DoctorSearchNavigation from './src/routes/DoctorSearchNavigation.js';
 
-AppRegistry.registerComponent(appName, () => DoctorSearchNavigation);
+AppRegistry.registerComponent(appName, () => StackNavigator);
