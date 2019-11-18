dfef485ef1b9a19091c85cc4b6249eddac0ef49e
Denmarc Rodriguez <denmarc.rodriguez@gmail.com>
2019-11-18 21:40:47 +0800

    merge update

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -62,7 +62,7 @@ export default class Dashboard extends React.Component {
               onPress={() => this.props.navigation.navigate('ProfilePage')}>
               <Thumbnail
                 small
-                source={require('../../assets/images/user.png')}
+                source={require('../../assets/images/nav-coordinator.png')}
                 resizeMode="contain"
               />
             </TouchableNativeFeedback>
