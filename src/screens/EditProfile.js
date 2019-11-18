e351b9543988b0564d25afc1059a4e025600a7bf
Denmarc Rodriguez <denmarc.rodriguez@gmail.com>
2019-11-18 21:36:09 +0800

    merge update from rynan

--- src/screens/EditProfile.js
+++ src/screens/EditProfile.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, Dimensions, View } from 'react-native'
 import { Container, Button, Text, Left, Body, Label, Item, Picker } from 'native-base'
 import { ScrollView } from 'react-native-gesture-handler';
 import { Input } from 'react-native-elements';
+import DatePicker from 'react-native-datepicker';
+import Spinner from 'react-native-spinkit';
+
+var { width, height } = Dimensions.get('window');
 
 const apiUpdateProfile = 'http://52.230.122.226:3000/api/v1/updateAccountInfo/';
 
@@ -10,16 +14,18 @@ export default class EditProfile extends React.Component {
 
     constructor(props) {
         super(props)
+        const { first_name, last_name, gender, email, birth_date, id } = this.props.navigation.state.params;
         this.state = {
-            first_name: '',
-            last_name: '',
-            gender: '',
-            id: '',
+            fname: first_name,
+            lname: last_name,
+            ugender: gender,
+            userId: id,
+            date: "2016-05-15",
+            isLoading: false,
         }
     }
 
     render() {
-        const { first_name, last_name, gender, email, birth_date, id } = this.props.navigation.state.params;
 
         return (
             <Container>
@@ -31,9 +37,9 @@ export default class EditProfile extends React.Component {
                             </Left>
                             <Body style={{ alignItems: 'flex-end', flex: 3 }}>
                                 <Input style={styles.itemInfo}
-                                    onChangeText={(first_name) => { this.setState({ first_name: first_name }) }}>
-                                    {first_name}
-                                </Input>
+                                    onChangeText={(fname) => this.setState({ fname })}
+                                    value={this.state.fname}
+                                />
                             </Body>
                         </Item>
                         <Item style={styles.itemStyle}>
@@ -42,10 +48,9 @@ export default class EditProfile extends React.Component {
                             </Left>
                             <Body style={{ alignItems: 'flex-end', flex: 3 }}>
                                 <Input style={styles.itemInfo}
-                                    autoCapitalize
-                                    onChangeText={last_name => this.setState({ last_name })}
-                                >{last_name}
-                                </Input>
+                                    onChangeText={(lname) => this.setState({ lname })}
+                                    value={this.state.lname}
+                                />
                             </Body>
                         </Item>
                         <Item style={styles.itemStyle}>
@@ -56,10 +61,10 @@ export default class EditProfile extends React.Component {
                             <Body style={{ alignItems: 'flex-end', flex: 3 }}>
                                 <Item picker>
                                     <Picker
-                                        selectedValue={this.state.gender}
+                                        selectedValue={this.state.ugender}
                                         style={{ height: 50, width: 100 }}
                                         onValueChange={(itemValue) =>
-                                            this.setState({ gender: itemValue })
+                                            this.setState({ ugender: itemValue })
                                         }>
                                         <Picker.Item label="Male" value="male" />
                                         <Picker.Item label="Female" value="female" />
@@ -72,27 +77,44 @@ export default class EditProfile extends React.Component {
                             <Left>
                                 <Label style={styles.itemLabel}>Birth Date</Label>
                             </Left>
-                            <Body style={{ alignItems: 'flex-end', flex: 3 }}>
-                                <Text style={styles.itemInfo}>{birth_date}</Text>
+                            <Body style={{ alignItems: 'flex-end', flex: 3, fontSize: 18, marginLeft: 10, marginTop: 10 }}>
+                                <Text style={styles.itemInfo}></Text>
+                                <DatePicker
+                                    defaultDate={new Date(2018, 4, 4)} style={{ alignSelf: 'flex-end' }}
+                                    locale={"en"}
+                                    timeZoneOffsetInMinutes={undefined}
+                                    modalTransparent={true}
+                                    animationType={"fade"}
+                                    androidMode={"default"}
+                                    placeHolderText="Select date"
+                                    textStyle={{ color: "#2d2d2d" }}
+                                    placeHolderTextStyle={{ color: "#bdc3c7" }}
+                                    disabled={false}
+                                    onDateChange={(date) => { this.setState({ date: date }) }}
+                                />
                             </Body>
                         </Item>
                     </View>
                     <View>
+
+
                     </View>
                     <View style={{ flex: 1 }}>
 
                         <Button rounded success block style={{ marginHorizontal: 20, marginTop: 20 }}
                             onPress={() => {
                                 let params = {
-                                    first_name: this.state.first_name,
-                                    last_name: this.state.last_name,
-                                    gender: this.state.gender
+                                    first_name: this.state.fname,
+                                    last_name: this.state.lname,
+                                    gender: this.state.ugender
                                 };
-                                updateProfile(params, id).then((result) => {
+                                console.log('params ito', params);
+                                updateProfile(params, this.state.userId).then((result) => {
+                                    this.state.isLoading = false;
                                     console.log(result, 'result ito');
                                     if (result.message === "Successfully updated account information.") {
                                         alert(result.message);
-                                        this.props.navigation.navigate('DashboardPage');
+                                        this.props.navigation.navigate('ProfilePage');
                                     }
                                 }).catch((error) => {
                                     console.log(`error = ${error}`);
@@ -106,12 +128,33 @@ export default class EditProfile extends React.Component {
             </Container>
         );
     };
+
 }
 
-async function updateProfile(params, id) {
 
+    // componentDidUpdate() {
+    //     const { spinnerStyle, spinnerTextStyle } = styles
+    //     if (this.state.isLoading) {
+    //         return (
+    //             <View style={spinnerStyle}>
+    //                 <Spinner
+    //                     color={'green'}
+    //                     size={50}
+    //                     type={'Wave'}
+    //                 />
+    //                 <Text style={spinnerTextStyle}>Saving...</Text>
+    //             </View>
+    //         )
+    //     }
+    // }
+
+
+async function updateProfile(params, uid) {
+
+    console.log(params)
+    console.log(uid)
     try {
-        let response = await fetch(apiUpdateProfile + id, {
+        let response = await fetch(apiUpdateProfile + uid, {
             method: 'PUT',
             headers: {
                 'Accept': 'application/json',
@@ -158,6 +201,18 @@ const styles = StyleSheet.create(
             alignSelf: "center",
             color: "#fff",
             fontSize: 14
+        },
+        spinnerStyle: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'absolute',
+            opacity: 0.2,
+            backgroundColor: 'black',
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
         }
     }
 )
